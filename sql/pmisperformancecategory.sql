b1283de2239a6f388aa372197d56f08ec4d9be29
RUFINO <aguilarufino@gmail.com>
2018-03-12 16:45:53 +0800

    initial commit

--- sql/pmisperformancecategory.sql
+++
@@ -1,9 +0,0 @@
-[getList]
-SELECT * 
-FROM pmis_performance_category
-WHERE ( code LIKE $P{searchtext} OR name LIKE $P{searchtext} )
-ORDER BY name
-
-[findById]
-SELECT * FROM pmis_performance_category WHERE objid = $P{objid}
-
